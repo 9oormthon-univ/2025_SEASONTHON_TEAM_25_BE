12829668071dac081b7952b0993e39558661327d
gihhyeon <fineblue.kh@gmail.com>
2025-09-12 23:26:55 +0900

    refactor: 롬복 사용

--- main-server/src/main/java/com/freedom/saving/application/read/SavingProductOptionItem.java
+++ main-server/src/main/java/com/freedom/saving/application/read/SavingProductOptionItem.java
@@ -1,10 +1,15 @@
 package com.freedom.saving.application.read;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
 /**
  * 상세 옵션(기간/금리) dto
  */
+@Getter
+@Setter
 public class SavingProductOptionItem {
 
     private Integer termMonths;
@@ -13,15 +18,4 @@ public class SavingProductOptionItem {
     private String rateType;
     private String rateTypeName;
 
-    public Integer getTermMonths() { return termMonths; }
-    public BigDecimal getRate() { return rate; }
-    public BigDecimal getRatePreferential() { return ratePreferential; }
-    public String getRateType() { return rateType; }
-    public String getRateTypeName() { return rateTypeName; }
-
-    public void setTermMonths(Integer termMonths) { this.termMonths = termMonths; }
-    public void setRate(BigDecimal rate) { this.rate = rate; }
-    public void setRatePreferential(BigDecimal ratePreferential) { this.ratePreferential = ratePreferential; }
-    public void setRateType(String rateType) { this.rateType = rateType; }
-    public void setRateTypeName(String rateTypeName) { this.rateTypeName = rateTypeName; }
 }
