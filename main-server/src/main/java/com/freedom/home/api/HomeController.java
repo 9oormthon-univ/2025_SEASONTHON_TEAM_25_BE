518c55cd42736252e5185dafc2afab448cb548c5
노영오 <nohyo05@naver.com>
2025-09-14 14:31:12 +0900

    Home Domain - Attendance Domain 분리

--- main-server/src/main/java/com/freedom/home/api/HomeController.java
+++ main-server/src/main/java/com/freedom/home/api/HomeController.java
@@ -1,14 +1,12 @@
 package com.freedom.home.api;
 
 import com.freedom.common.security.CustomUserPrincipal;
-import com.freedom.home.api.response.AttendanceResponse;
 import com.freedom.home.api.response.HomeResponse;
 import com.freedom.home.application.HomeFacadeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,10 +20,5 @@ public class HomeController {
     @GetMapping
     public ResponseEntity<HomeResponse> getHome(@AuthenticationPrincipal CustomUserPrincipal principal) {
         return ResponseEntity.ok(homeService.getMainHomeData(principal.getId()));
-    }
-
-    @PostMapping("/attendance")
-    public ResponseEntity<AttendanceResponse> markAttendance(@AuthenticationPrincipal CustomUserPrincipal principal) {
-        return ResponseEntity.ok(homeService.markAttendance(principal.getId()));
     }
 }
